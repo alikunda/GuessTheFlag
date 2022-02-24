86b41aa6effaf7b61669833e2e69022d8e5d3dac
Mohammed Ali Kunda <mkund01@jaguar.tamu.edu>
2022-02-24 16:08:24 -0600

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Mohammed on 2/24/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
